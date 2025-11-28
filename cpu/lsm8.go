bea359f96e4a9143010e7519ecda2b3fea8554a0
Javier Acevedo <javier.acevedo233@gmail.com>
2025-11-28 13:48:24 -0400

    Implement MOV_X_M general function (MOV with indirect memory access)

--- cpu/lsm8.go
+++ cpu/lsm8.go
@@ -1,8 +1,21 @@
 package cpu
 import "fmt"
+import "8080/memory"
 
 func MOV(left Reg, right Reg) {
-	fmt.Printf("%02X <- %02X\n", left, right)
+	fmt.Printf("MOV: %02X <- %02X\n", left, right)
 	REGISTERS[left] = REGISTERS[right]
+}
+
+func MOV_X_M(x Reg) {
+	MSB := REGISTERS[H]
+	LSB := REGISTERS[L]
+	addr := (uint16(MSB) << 8) | uint16(LSB)
+
+	value := memory.MEMORY[addr]
+
+	fmt.Printf("MOV_X_M: %02X <- %02X (addr: 0x%04X)\n", x, value, addr)
+
+	REGISTERS[x] = value
 }
 
