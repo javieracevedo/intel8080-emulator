701b1a99c4c4c4998447a6ee0febfa5b6ec5276f
Javier Acevedo <javier.acevedo233@gmail.com>
2025-12-13 21:44:01 -0400

    feat: add `ADD_X` instruction test and refactor `MOV_X_M` test with subtests.

--- cpu/cpu_test.go
+++ cpu/cpu_test.go
@@ -121,9 +121,45 @@ func TestMOV_X_M(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		MOV_X_M(tt.reg)
-		if memory.MEMORY[0x0101] != 0x2 {
-			t.Fatalf("got 0x%02X, want 0x%02X", memory.MEMORY[0x0101], 0x2)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			MOV_X_M(tt.reg)
+			if memory.MEMORY[0x0101] != 0x2 {
+				t.Fatalf("got 0x%02X, want 0x%02X", memory.MEMORY[0x0101], 0x2)
+			}
+		})
+	}
+}
+
+func TestADD_X(t *testing.T) {
+	REGISTERS = [7]byte{0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x0}
+
+	t.Cleanup(func() {
+		resetRegisters()
+		resetMemory()
+	})
+
+	var tests = []struct {
+		name string
+		reg  Reg
+		want byte
+	}{
+		{"ADD B", B, REGISTERS[B] + REGISTERS[A]},
+		{"ADD C", C, REGISTERS[C] + REGISTERS[A]},
+		{"ADD D", D, REGISTERS[D] + REGISTERS[A]},
+		{"ADD E", E, REGISTERS[E] + REGISTERS[A]},
+		{"ADD H", H, REGISTERS[H] + REGISTERS[A]},
+		{"ADD L", L, REGISTERS[L] + REGISTERS[A]},
+		{"ADD A", A, REGISTERS[A] + REGISTERS[A]},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			REGISTERS = [7]byte{0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x0}
+
+			ADD_X(tt.reg)
+			if REGISTERS[A] != tt.want {
+				t.Fatalf("got 0x%02X, want 0x%02X", REGISTERS[A], tt.want)
+			}
+		})
 	}
 }
